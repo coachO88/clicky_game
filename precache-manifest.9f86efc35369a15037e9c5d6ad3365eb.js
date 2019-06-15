8885c0d63c1c546e826665abcbf5771706047194
coachO88 <rangel52088@gmail.com>
2019-06-15 13:53:02 -0700

    Updates

---
+++ precache-manifest.9f86efc35369a15037e9c5d6ad3365eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d97e68fe88417db289f6911fd2e91289",
+    "url": "/clicky_game/index.html"
+  },
+  {
+    "revision": "8bd993f33acc829f9fbb",
+    "url": "/clicky_game/static/css/main.46a2c035.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/clicky_game/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "8bd993f33acc829f9fbb",
+    "url": "/clicky_game/static/js/main.17edff8f.chunk.js"
+  },
+  {
+    "revision": "ef6ad64b0df3315f3b6a",
+    "url": "/clicky_game/static/js/runtime~main.33627756.js"
+  }
+]);
